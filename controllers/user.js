7fff3581bfa108237236611c93fe70179cb3c60e
Atharv sinha <87602922+OKa445e@users.noreply.github.com>
2025-01-06 20:03:43 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -8,29 +8,27 @@ import ErrorHandler from "../middleware/err.js";
 export const getRegister = async (req, res,next) => {
     try {
         const { name, email, password } = req.body;
-
+    
         let user = await User.findOne({ email });
-
-        if (user)
-            return next(new ErrorHandler("User Already Exist", 400));
-
+    
+        if (user) return next(new ErrorHandler("User Already Exist", 400));
+    
         const hashedPassword = await bcrypt.hash(password, 10);
-
+    
         user = await User.create({ name, email, password: hashedPassword });
-        setCookies(user, res, "Registered Succesfully", 201);
-
-    } catch (error) {
+    
+        setCookies(user, res, "Registered Successfully", 201);
+      } catch (error) {
         next(error);
-    }
+      };
 };
-
 
 export const getMyProfile = (req, res) => {
 
     res.status(200).json({
         success: true,
         user: req.user,
-    });
+      });
 
 };
 
@@ -41,17 +39,14 @@ export const getLogin = async (req, res, next) => {
         const user = await User.findOne({ email }).select("+password");
 
         if (!user)
-            return next(new ErrorHandler("Invalid email or Password", 404));
+            return next(new ErrorHandler("Invalid email or Password", 400));
 
 
         const isMatch = await bcrypt.compare(password, user.password);
 
 
         if (!isMatch)
-            return res.status(400).json({
-                success: false,
-                message: "Invalid Email or Pasword",
-            });
+            return  next(new ErrorHandler("Invalid email or Password", 400));
         setCookies(user, res, `Welcome back, ${user.name}`, 200)
     } catch (error) {
         next(error);
@@ -61,15 +56,15 @@ export const getLogin = async (req, res, next) => {
 export const getLogout = (req, res) => {
 
     res
-        .status(200)
-        .cookie("token", "", {
-            expires: new Date(Date.now()),
-            sameSite: process.env.NODE_ENV === "Developement" ? "lax" : "none",
-            secure: process.env.NODE_ENV === "Developement" ? false : true,
-        })
-        .json({
-            success: true,
-            user: req.user,
-        });
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
+    })
+    .json({
+      success: true,
+      user: req.user,
+    });
 
 }
